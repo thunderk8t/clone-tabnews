2ca59deaa531638b78b9bd04ac5847dd64ba8486
Catarina <cds6am@gmail.com>
2024-01-21 13:13:40 +0000

    adiciona scripts `lint:check` `lint:fix` e prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementando o desafio do tabnews do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-   return <><h1>Plot twist.</h1>
-   <h1>Estamos num conto do Kafka e amanhã, quem vai amanhecer em cima da cama sou eu, a barata</h1></>
+function Home() {
+  return (
+    <>
+      <h1>Plot twist.</h1>
+      <h1>
+        Estamos num conto do Kafka e amanhã, quem vai amanhecer em cima da cama
+        sou eu, a barata
+      </h1>
+    </>
+  );
 }
+
 export default Home;
