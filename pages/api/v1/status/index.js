93051a026cd14ee169be18a16b0fcfdc53e2d472
Catarina <cds6am@gmail.com>
2024-01-27 21:51:31 +0000

    add status endpoint final stuff and database sanitizing and error handling

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result);
-  response.status(200).json({ chave: "as mais mais" });
+  const updatedAt = new Date().toISOString();
+  const pg_version = await database.query("SELECT version();");
+  const all_configs = await database.query("SHOW max_connections;");
+  const dbName = process.env.POSTGRES_DB;
+  const all_pg_stat_activity = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const postgres_version = pg_version.rows[0].version.substring(11, 15);
+  const max_connections = parseInt(all_configs.rows[0].max_connections);
+  const used_connections = all_pg_stat_activity.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgres_version,
+        max_connections: max_connections,
+        used_connections: used_connections,
+      },
+    },
+  });
 }
 
 export default status;
